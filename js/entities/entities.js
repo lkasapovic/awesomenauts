9a4932e6378333b7215d380dee558c90183fe2d1
Lejla Kaasapovic <lakergirl.lk@gmail.com>
2015-03-11 09:18:53 -0700

    no message

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -48,3 +48,83 @@ game.PlayerEntity = me.Entity.extend({
 
     }
 });
+
+game.PlayerBaseEntity = me.Entity.extend({
+    init : function(x, y, settings) {
+        this._super(me.Entity, 'init', [x, y, {
+            image: "tower",
+            width: 100,
+            height: 100,
+            spritewidth: "100",
+            spriteheight: "100",
+            getShape: function() {
+                return (new me.Rect(0, 0, 100, 100)).toPolygon();
+            }
+        }]);
+        this.broken = false;
+        this.health = 10;
+        this.alwaysUpdate = true;
+        this.body.onCollision = this.onCollision.bind(this);
+        
+        this.type = "PlayerBaseEntity";
+        this.renderable.addAnimation("broken", [1]);
+        this.renderable.addAnimation("idle", [0]);
+        
+        this.renderable.setCurrentAnimation("idle");
+    },
+    
+    update:function (delta) {
+        if(this.health<=0) {
+            this.broken = true;
+            this.renderable.setCurrentAnimation("broken");
+        }
+        this.body.update(delta);
+        
+        this._super(me.Entity, "update", [delta]);
+        return true;
+    },
+    
+    
+    onCollision: function(){
+        
+    }
+    
+    
+});
+
+game.EnemyBaseEntity = me.Entity.extend({
+    init : function(x, y, settings) {
+        this._super(me.Entity, 'init', [x, y, {
+            image: "tower",
+            width: 100,
+            height: 100,
+            spritewidth: "100",
+            spriteheight: "100",
+            getShape: function() {
+                return (new me.Rect(0, 0, 100, 100)).toPolygon();
+            }
+        }]);
+        this.broken = false;
+        this.health = 10;
+        this.alwaysUpdate = true;
+        this.body.onCollision = this.onCollision.bind(this);
+        
+        this.type = "PlayerBaseEntity";
+    },
+    
+    update:function (delta) {
+        if(this.health<=0) {
+            this.broken = true;
+        }
+        this.body.update(delta);
+        
+        this._super(me.Entity, "update", [delta]);
+        return true;
+    },
+    
+    onCollision: function(){
+        
+    }
+    
+    
+});
