d762b82cbf823d0c31afae3bad9ec20ebb3ba83b
Lejla Kaasapovic <lakergirl.lk@gmail.com>
2015-03-26 09:16:29 -0700

    no message

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -14,7 +14,8 @@ game.PlayerEntity = me.Entity.extend({
                     return(new me.Rect(0, 0, 64, 64)).toPolygon();
                 }
             }]);
-
+        this.type = "PlayerEntity";
+        this.health = 100;
         this.body.setVelocity(5, 20);
         // keeps track of the direction 
         this.facing = "right";
@@ -29,6 +30,7 @@ game.PlayerEntity = me.Entity.extend({
 
         this.renderable.setCurrentAnimation("idle");
     },
+    
     update: function(delta) {
         this.now = new Date().getTime();
         if (me.input.isKeyPressed("right")) {
@@ -79,6 +81,11 @@ game.PlayerEntity = me.Entity.extend({
         this._super(me.Entity, "update", [delta]);
         return true;
     },
+    
+    loseHealth: function(damage) {
+        this.health = this.health - damage;
+    },
+    
     collideHandler: function(response) {
         if (response.b.type === 'EnemyBaseEntity') {
             var ydif = this.pos.y - response.b.pos.y;
@@ -229,6 +236,7 @@ game.EnemyCreep = me.Entity.extend({
         this.renderable.addAnimation("walk", [3, 4, 5], 80);
         this.renderable.setCurrentAnimation("walk");
     },
+    
     update: function(delta) {
         this.now = new Date().getTime();
 
@@ -244,22 +252,45 @@ game.EnemyCreep = me.Entity.extend({
         this._super(me.Entity, "update", [delta]);
         return true;
     },
+    
     collideHandler: function(response) {
-        if (response.b.type==='PlayerBase') {
-            this.attacking=true;
-            // this.lastAttacking = this.now;
+        console.log(response.b.type);
+        if (response.b.type === 'PlayerBaseEntity') {
+            this.attacking = true;
+            this.lastAttacking = this.now;
             this.body.vel.x = 0;
             // moves creep to the right
             this.pos.x = this.pos.x + 1;
             // checks that it has been at least one second since the creep hit a base
-            if ((this.now-this.lastHit >= 1000)) {
+            if ((this.now - this.lastHit >= 1000)) {
                 // update the lasthit timer
                 this.lastHit = this.now;
                 //makes player base call its loseHealth function and passs it a 
                 // damage of one
                 response.b.loseHealth(1);
             }
+
+        } else if (response.b.type === 'PlayerEntity') {
+            var xdif = this.pos.x - response.b.pos.x;
+            
+            this.attacking = true;
+            // this.lastAttacking = this.now;
+
+            if(xdif>0) {
+                this.pos.x = this.pos.x + 1;
+                // moves creep to the right
+                this.body.vel.x = 0;
+            }  
+            // checks that it has been at least one second since the creep hit a player
+            if ((this.now - this.lastHit >= 1000) && xdif>0) {
+                // update the lasthit timer
+                this.lastHit = this.now;
+                //makes player call its loseHealth function and passs it a 
+                // damage of one
+                response.b.loseHealth(1);
+            }
         }
+
     }
 });
 
