b3a417b4969c6d93edede44bca68babdbfba9414
Lejla Kaasapovic <lakergirl.lk@gmail.com>
2015-04-02 09:18:45 -0700

    #wcw on a Thursday @sophiabush

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -16,6 +16,7 @@ game.PlayerEntity = me.Entity.extend({
 
         this.renderable.setCurrentAnimation("idle");
     },
+    
     setSuper: function() {
         this._super(me.Entity, 'init', [x, y, {
                 image: "player",
@@ -30,57 +31,88 @@ game.PlayerEntity = me.Entity.extend({
                 }
             }]);
     },
+    
     setPlayerTimers: function() {
         this.now = new Date().getTime();
         this.lastHit = this.now;
         this.lastAttack = new Date().getTime();
     },
+    
     setAttributes: function() {
         this.health = game.data.playerHealth;
         this.body.setVelocity(5, 20);
         this.attack = game.data.playerAttack;
     },
+    
     setFlags: function() {
         // keeps track of the direction 
         this.facing = "right";
         this.dead = false;
+        this.attacking  = false;
     },
+    
     addAnimation: function() {
         this.renderable.addAnimation("idle", [78]);
         this.renderable.addAnimation("walk", [117, 118, 119, 120, 121, 122, 123, 124, 125]);
         this.renderable.addAnimation("attack", [65, 66, 67, 68, 69, 70, 71, 72], 80);
     },
+    
     update: function(delta) {
         this.now = new Date().getTime();
-
+        this.dead = checkifDead();
+        this.checkKeyPressesAndMove();
+        this.setAnimation();
+        me.collision.check(this, true, this.collideHandler.bind(this), true);
+        this.body.update(delta);
+        this._super(me.Entity, "update", [delta]);
+        return true;
+    },
+    
+    checkIfDead: function() {
         if (this.health <= 0) {
-            this.dead = true;
+            return true;
         }
-
-
+        return false;
+    },
+    
+    checkKeyPressesAndMove: function() {
         if (me.input.isKeyPressed("right")) {
-            // adds to the position of my x by the velocity defined above in 
-            //setVelocity() and multiplying it by me.timer.tick.
-            //me.timer.tick makes the movement look smooth
-            this.body.vel.x += this.body.accel.x * me.timer.tick;
-            this.facing = "right";
-            this.flipX(true);
+            this.moveRight();
         } else if (me.input.isKeyPressed("left")) {
-            this.facing = "left";
-            this.body.vel.x -= this.body.accel.x * me.timer.tick;
-            this.flipX;
-            (false);
+            this.moveleft();
         } else {
             this.body.vel.x = 0;
         }
 
         if (me.input.isKeyPressed("jump") && !this.jumping && !this.body.falling) {
-            this.body.jumping = true;
-            this.body.vel.y -= this.body.accel.y * me.timer.tick;
+            this.jump();
         }
-
-
-        if (me.input.isKeyPressed("attack")) {
+        
+        this.attacking = me.input.isKeyPressed("attack")
+    },
+   
+    moveRight: function() {
+        // adds to the position of my x by the velocity defined above in 
+        //setVelocity() and multiplying it by me.timer.tick.
+        //me.timer.tick makes the movement look smooth
+        this.body.vel.x += this.body.accel.x * me.timer.tick;
+        this.facing = "right";
+        this.flipX(true);
+    },
+    
+    moveLeft: function() {
+        this.facing = "left";
+        this.body.vel.x -= this.body.accel.x * me.timer.tick;
+        this.flipX(false);
+    },
+    
+    jump: function() {
+        this.body.jumping = true;
+        this.body.vel.y -= this.body.accel.y * me.timer.tick;
+    },
+    
+    setAnimation: function() {
+        if (this.attacking) {
             if (!this.renderable.isCurrentAnimation("attack")) {
                 //sets the current animationto attack and one that is over goes
                 //back to the idle animation
@@ -99,16 +131,12 @@ game.PlayerEntity = me.Entity.extend({
         } else if (!this.renderable.isCurrentAnimation("attack")) {
             this.renderable.setCurrentAnimation("idle");
         }
-
-        me.collision.check(this, true, this.collideHandler.bind(this), true);
-        this.body.update(delta);
-
-        this._super(me.Entity, "update", [delta]);
-        return true;
     },
+    
     loseHealth: function(damage) {
         this.health = this.health - damage;
     },
+    
     collideHandler: function(response) {
         if (response.b.type === 'EnemyBaseEntity') {
             var ydif = this.pos.y - response.b.pos.y;
