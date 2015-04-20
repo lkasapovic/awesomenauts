0770edc4d3ae6e006349a8b0931e60cce8fdbee8
Lejla Kaasapovic <lakergirl.lk@gmail.com>
2015-04-20 10:42:24 -0700

    #mcm @1van5algado

--- js/screens/title.js
+++ js/screens/title.js
@@ -18,7 +18,7 @@ game.TitleScreen = me.ScreenObject.extend({
             update: function(dt) {
                 return true;
             },
-            newGame: function() { 
+            newGame: function() {
                 me.input.releasePointerEvent('pointerdown', this);
                 me.save.remove('exp');
                 me.save.remove('exp1');
@@ -29,12 +29,12 @@ game.TitleScreen = me.ScreenObject.extend({
                 me.state.change(me.state.PLAY);
             }
         })));
-        
-         me.game.world.addChild(new (me.Renderable.extend({
+
+        me.game.world.addChild(new (me.Renderable.extend({
             init: function() {
                 this._super(me.Renderable, 'init', [380, 340, 250, 50]);
                 this.font = new me.Font("Arial", 46, "white");
-                 me.input.registerPointerEvent('pointerdown', this, this.newGame.bind(this), true);
+                me.input.registerPointerEvent('pointerdown', this, this.newGame.bind(this), true);
             },
             draw: function(renderer) {
                 this.font.draw(renderer.getContext(), "CONTINUE", this.pos.x, this.pos.y);
@@ -53,7 +53,7 @@ game.TitleScreen = me.ScreenObject.extend({
             }
         })));
 
-   },
+    },
     /**	
      *  action to perform when leaving this screen (state change)
      */
