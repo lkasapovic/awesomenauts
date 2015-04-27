ffb425aae1071883815f4d0035f3ba04b58fee50
Lejla Kaasapovic <lakergirl.lk@gmail.com>
2015-04-27 08:51:45 -0700

    no message

--- js/screens/loadProfile.js
+++ js/screens/loadProfile.js
@@ -4,6 +4,11 @@ game.LoadProfile = me.ScreenObject.extend({
      */
     onResetEvent: function() {
         me.game.world.addChild(new me.Sprite(0, 0, me.loader.getImage('load-screen')), -10); //TODO
+        
+        document.getElementById("input").style.visibility = "visible";
+        document.getElementById("load").style.visibility = "visible";
+        
+
 
         me.input.unbindKey(me.input.KEY.B);
         me.input.unbindKey(me.input.KEY.Q);
@@ -30,7 +35,9 @@ game.LoadProfile = me.ScreenObject.extend({
      *  action to perform when leaving this screen (state change)
      */
     onDestroyEvent: function() {
-
+        document.getElementById("input").style.visibility = "visible";
+        document.getElementById("load").style.visibility = "visible";
+        
     }
 });
 
